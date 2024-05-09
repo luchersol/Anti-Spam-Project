ea89497702d739ccb6563fad293c8454fbe0843e
luchersol <lmherencia2003@gmail.com>
2024-05-09 10:21:10 +0200

    Subir proyecto

---
+++ src/main/java/acme/components/AntiSpamSystemRepository.java
@@ -0,0 +1,18 @@
+
+package acme.components;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+import acme.client.repositories.AbstractRepository;
+
+public interface AntiSpamSystemRepository extends AbstractRepository {
+
+	@Query("select s.word from Spam s")
+	List<String> findAllSpam();
+
+	@Query("select sys.threshold from AntiSpamSystem sys")
+	Double findThresholdSpam();
+
+}
